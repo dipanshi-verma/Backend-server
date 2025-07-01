55e0ea3dc5e555192e0965c2e047346ead1b1d7c
Dipanshi Verma <dipanshiverma0@gmail.com>
2025-07-01 12:46:56 +0530

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 import cors from 'cors'
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://frontend-client-nu.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials:true
     }
